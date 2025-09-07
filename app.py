0daf7c573206e4cde2f518de298c1c14cad3a6c6
Nitesh <nitesh@cresol.in>
2025-09-07 12:26:20 +0530

    innitial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.get("/api/hello")
+def hello():
+    return jsonify(service="api", branch="develop", msg="Hello from API! New 1")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
